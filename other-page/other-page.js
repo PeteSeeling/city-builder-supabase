abb787c138ea34c52b43760aff43174e7ca5fd4c
Pete Seeling <peteseeling@gmail.com>
2022-01-04 19:10:11 -0600

    html done. dom grabbed. working on functions

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,9 +1,24 @@
 import { checkAuth, logout } from '../fetch-utils.js';
 
-checkAuth();
+// checkAuth();
 
 const logoutButton = document.getElementById('logout');
+const waterfrontImgEl = document.querySelector('.water-img');
+const skylineImgEl = document.querySelector('.skyline-img');
+const castleImgEl = document.querySelector('.castle-img');
+const sloganForm = document.querySelector('.slogan-form');
+const nameForm = document.querySelector('.name-form');
+const waterfrontDropdown = document.querySelector('#water-dropdown');
+const skylineDropdown = document.querySelector('#skyline-dropdown');
+const castleDropdown = document.querySelector('#castle-dropdown');
+const sloganListEl = document.querySelector('.slogan-list');
+
+
+
+
 
 logoutButton.addEventListener('click', () => {
     logout();
 });
+
+
